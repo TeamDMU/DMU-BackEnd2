bf5457355d4f6bed6338d3c43f3ff508d79ac954
GiJungPark <weeds2577@gmail.com>
2024-01-09 13:49:32 +0900

    style(WebPageLoaderTest): 팩토리 메서드 패턴으로 변경함에 따라 테스트 코드 수정

--- src/test/java/me/gijung/DMforU/utils/WebPageLoaderTest.java
+++ src/test/java/me/gijung/DMforU/utils/WebPageLoaderTest.java
@@ -7,18 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WebPageLoaderTest {
 
-    WebPageLoader webPageLoader = new WebPageLoader();
-
     @DisplayName("HTML 정보를 가져오는지 테스트")
     @Test
     void getHTML() {
-        assertThat(webPageLoader.getHTML("https://www.google.com/")).isNotNull();
+        assertThat(WebPageLoader.getHTML("https://www.google.com/")).isNotNull();
     }
 
     @DisplayName("존재하지 않는 URL 요청 테스트")
     @Test
     void getInvalidHTML() {
-        assertThat(webPageLoader.getHTML("https://empty")).isNull();
+        assertThat(WebPageLoader.getHTML("https://empty")).isNull();
     }
 
 }
