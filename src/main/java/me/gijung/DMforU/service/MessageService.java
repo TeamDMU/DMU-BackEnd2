cd505da122f3a36329030472b68fc82d11d183d3
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-01-27 18:08:53 +0900

    FCM 서버 토큰 업데이트/삭제 인터페이스 상속 및 로직 수정

--- src/main/java/me/gijung/DMforU/service/MessageService.java
+++ src/main/java/me/gijung/DMforU/service/MessageService.java
@@ -2,35 +2,33 @@ package me.gijung.DMforU.service;
 
 import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
+import me.gijung.DMforU.model.dto.TokensDto;
 import me.gijung.DMforU.service.Mesaage.FirebaseMessagingService;
-import me.gijung.DMforU.service.Mesaage.Messaging;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class MessageService{
+public class MessageService implements TokenService<TokensDto> {
 
     private final FirebaseMessagingService<FirebaseMessaging> firebaseMessaging;
 
     public void send_message(String topic, String title, String body) throws FirebaseMessagingException {
         Message message = firebaseMessaging.sendMessage(topic, title, body);
-        String send = FirebaseMessaging.getInstance().send(message);
-        System.out.println("send = " + send);
-
+        FirebaseMessaging.getInstance().send(message);
     }
 
-    public void update_topic(List<String> tokens, String topic) throws FirebaseMessagingException {
-        TopicManagementResponse topicManagementResponse = firebaseMessaging.getInstance().subscribeToTopic(tokens, topic);
+    public void updateToken(String topic, List<String> Tokens) throws FirebaseMessagingException {
+        TopicManagementResponse topicManagementResponse = firebaseMessaging.getInstance().subscribeToTopic(Tokens, topic);
         System.out.println("업데이트 성공갯수 :: " + topicManagementResponse.getSuccessCount());
         System.out.println("업데이트 실패갯수 :: " + topicManagementResponse.getFailureCount());
     }
-    public void delete_topic(List<String> tokens, String topic) throws FirebaseMessagingException {
-        TopicManagementResponse topicManagementResponse = firebaseMessaging.getInstance().unsubscribeFromTopic(tokens, topic);
+
+    public void deleteToken(String topic, List<String> Tokens) throws FirebaseMessagingException {
+        TopicManagementResponse topicManagementResponse = firebaseMessaging.getInstance().unsubscribeFromTopic(Tokens, topic);
         System.out.println("삭제 성공갯수 :: " + topicManagementResponse.getSuccessCount());
         System.out.println("삭제 실패갯수 :: " + topicManagementResponse.getFailureCount());
-
     }
+
 }
