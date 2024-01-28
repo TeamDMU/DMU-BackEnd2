7817d8771d0d341106f364ce2db579532acc99eb
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-01-28 15:43:20 +0900

    스케쥴 어노테이션 활성 어노테이션 추카

--- src/main/java/me/gijung/DMforU/Application.java
+++ src/main/java/me/gijung/DMforU/Application.java
@@ -2,8 +2,11 @@ package me.gijung.DMforU;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
