b57bda268d440164a3c12687010c8139b172fc1a
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-02-03 17:56:36 +0900

    파일 이름 변경 및 주석 추가, TimeStamp(데이터 타입 변경 및 미리초 삭제)

--- src/main/java/me/gijung/DMforU/service/RedisTokenService.java
+++ src/main/java/me/gijung/DMforU/service/RedisTokenService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
@@ -21,25 +22,27 @@ public class RedisTokenService implements TokenService<TokensDto> {
 
     private RedisTemplate<String, String> redisTemplate;
     //Redis 기기별 토큰 타임스탬프 업데이트
-    //Token 최초 등록
+    //Redis Server Token 최초 등록
     public void save(TokensDto tokensDto) {
         try {
             for (String token : tokensDto.getTokens()) {
-                redisTemplate.opsForValue().set(token, String.valueOf(LocalDateTime.now()), 10, TimeUnit.SECONDS);       }
+                //초 뒤에 미리초 삭제
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                redisTemplate.opsForValue().set(token, LocalDateTime.now().format(formatter), 40, TimeUnit.SECONDS);       }
         } catch (Exception e) {
             System.out.println("Redis_Update_Error = " + e);
         }
     }
 
-    //Token 유효시간 갱신
+    //Redis Server Token 유효시간 갱신
     public void updateToken(TokensDto tokensDto) {
         for (String token : tokensDto.getTokens()) {
-            redisTemplate.expire(token, 10, TimeUnit.SECONDS);
+            redisTemplate.expire(token, 40, TimeUnit.SECONDS);
         }
     }
 
-    //사용자가 직접
-    //Redis 기기별 토큰 삭제
+
+    //Redis Server Token 삭제
     public void deleteToken(TokensDto tokensDto) {
         for (String token : tokensDto.getTokens()) {
             System.out.println("token = " + token);
