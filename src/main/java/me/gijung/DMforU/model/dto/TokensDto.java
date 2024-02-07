9dff1b919ab204dc282db8be4fea1f26df9a7750
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-02-07 19:04:25 +0900

    Topic 타입을 List 형식으로 변경

--- src/main/java/me/gijung/DMforU/model/dto/TokensDto.java
+++ src/main/java/me/gijung/DMforU/model/dto/TokensDto.java
@@ -3,13 +3,13 @@ package me.gijung.DMforU.model.dto;
 import lombok.Data;
 import me.gijung.DMforU.config.Topic;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class TokensDto {
 
     private List<String> tokens;
+    private List<Topic> topic;
 
-    private Topic topic;
+
 }
