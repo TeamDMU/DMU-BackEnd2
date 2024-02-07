466ff2caf502bd443f35bd97cac419e1823e8588
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-02-07 19:05:20 +0900

    Token TTL 만료시 자동삭제 버그 해결

--- src/main/java/me/gijung/DMforU/service/GoogleTokenService.java
+++ src/main/java/me/gijung/DMforU/service/GoogleTokenService.java
@@ -2,6 +2,7 @@ package me.gijung.DMforU.service;
 
 import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
+import me.gijung.DMforU.config.Topic;
 import me.gijung.DMforU.model.dto.TokensDto;
 import me.gijung.DMforU.service.Mesaage.FirebaseMessagingService;
 import org.springframework.stereotype.Service;
@@ -15,34 +16,33 @@ public class GoogleTokenService implements TokenService<TokensDto> {
     public void send_message(String topic, String title, String body) throws FirebaseMessagingException {
         Message message = firebaseMessaging.sendMessage(topic, title, body);
         FirebaseMessaging.getInstance().send(message);
+
+
     }
 
 
     //Google FCM서버에 기기별 구독 업데이트
     public void updateToken(TokensDto tokensDto) throws FirebaseMessagingException {
-        TopicManagementResponse topicManagementResponse = firebaseMessaging
-                .getInstance()
-                .subscribeToTopic(tokensDto.getTokens(), String.valueOf(tokensDto.getTopic()));
-        System.out.println("업데이트 성공갯수 :: " + topicManagementResponse.getSuccessCount());
+        FirebaseMessaging instance = firebaseMessaging
+                .getInstance();
 
-        // 오류발생시 오류 체크
-        if (topicManagementResponse.getFailureCount() != 0) {
-            System.out.println("Errors  :: " + topicManagementResponse.getErrors());
+        for (Topic topic : tokensDto.getTopic()) {
+            TopicManagementResponse topicManagementResponse = instance.subscribeToTopic(tokensDto.getTokens(), String.valueOf(topic));
+            System.out.println("업데이트 성공갯수 :: " +topicManagementResponse.getSuccessCount() );
         }
+
+
     }
 
 
     //Google FCM서버에 기기별 구독 삭제
     public void deleteToken(TokensDto tokensDto) throws FirebaseMessagingException {
-        TopicManagementResponse topicManagementResponse = firebaseMessaging
-                .getInstance()
-                .unsubscribeFromTopic(tokensDto.getTokens(), String.valueOf(tokensDto.getTopic()));
-        System.out.println("삭제 성공갯수 :: " + topicManagementResponse.getSuccessCount());
+        FirebaseMessaging instance = firebaseMessaging
+                .getInstance();
 
-        // 오류발생시 오류 체크
-        if (topicManagementResponse.getFailureCount() != 0) {
-            System.out.println("Errors  :: " + topicManagementResponse.getErrors());
+        for (Topic topic : Topic.values()) {
+            TopicManagementResponse topicManagementResponse = instance.unsubscribeFromTopic(tokensDto.getTokens(), String.valueOf(topic));
+            System.out.println("삭제 성공갯수 :: " +topicManagementResponse.getSuccessCount() );
         }
     }
-
 }
