fc68b07e17d75f38d8bddd942408f6e8ff81a56c
GiJungPark <weeds2577@gmail.com>
2024-02-09 02:09:22 +0900

    #8 feat(CrawlingController): 원하는 학과의 공지사항을 페이지네이션하는 Get API 구현

--- src/main/java/me/gijung/DMforU/controller/CrawlingController.java
+++ src/main/java/me/gijung/DMforU/controller/CrawlingController.java
@@ -3,10 +3,15 @@ package me.gijung.DMforU.controller;
 import lombok.RequiredArgsConstructor;
 import me.gijung.DMforU.model.domain.Diet;
 import me.gijung.DMforU.model.domain.schedule.YearSchedule;
+import me.gijung.DMforU.model.dto.DepartmentNoticeDto;
+import me.gijung.DMforU.service.DepartmentNoticeService;
 import me.gijung.DMforU.service.DietService;
 import me.gijung.DMforU.service.SchedulerService;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +23,7 @@ public class CrawlingController {
 
     private final DietService dietService;
     private final SchedulerService schedulerService;
+    private final DepartmentNoticeService departmentNoticeService;
 
     @GetMapping("/diet")
     public List<Diet> getDiet() {
@@ -27,5 +33,14 @@ public class CrawlingController {
     @GetMapping("/scheduler")
     public List<YearSchedule> getScheduler() {
         return schedulerService.getData();
+    }
+
+    @GetMapping("/departmentNotice/{department}")
+    public List<DepartmentNoticeDto> getDepartmentNotice(@PathVariable String department,
+                                                         @RequestParam(name = "page", defaultValue = "1") int page,
+                                                         @RequestParam(name = "size", defaultValue = "20") int size) {
+        List<DepartmentNoticeDto> pageDepartmentNotices = departmentNoticeService.findDepartmentNotices(page, size, department);
+
+        return pageDepartmentNotices;
     }
 }
