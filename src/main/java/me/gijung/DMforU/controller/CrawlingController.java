9b261fdc01d8431db672a4da6eb586f36a623f8f
GiJungPark <weeds2577@gmail.com>
2024-02-11 01:59:31 +0900

    feat(CrawlingController): 대학 공지사항을 페이지네이션하는 Get API 구현

--- src/main/java/me/gijung/DMforU/controller/CrawlingController.java
+++ src/main/java/me/gijung/DMforU/controller/CrawlingController.java
@@ -3,10 +3,11 @@ package me.gijung.DMforU.controller;
 import lombok.RequiredArgsConstructor;
 import me.gijung.DMforU.model.domain.Diet;
 import me.gijung.DMforU.model.domain.schedule.YearSchedule;
-import me.gijung.DMforU.model.dto.DepartmentNoticeDto;
+import me.gijung.DMforU.model.dto.NoticeDto;
 import me.gijung.DMforU.service.DepartmentNoticeService;
 import me.gijung.DMforU.service.DietService;
 import me.gijung.DMforU.service.SchedulerService;
+import me.gijung.DMforU.service.UniversityNoticeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ public class CrawlingController {
     private final DietService dietService;
     private final SchedulerService schedulerService;
     private final DepartmentNoticeService departmentNoticeService;
+    private final UniversityNoticeService universityNoticeService;
 
     @GetMapping("/diet")
     public List<Diet> getDiet() {
@@ -35,11 +37,19 @@ public class CrawlingController {
     }
 
     @GetMapping("/departmentNotice/{department}")
-    public List<DepartmentNoticeDto> getDepartmentNotice(@PathVariable String department,
-                                                         @RequestParam(name = "page", defaultValue = "1") int page,
-                                                         @RequestParam(name = "size", defaultValue = "20") int size) {
-        List<DepartmentNoticeDto> pageDepartmentNotices = departmentNoticeService.findDepartmentNotices(page, size, department);
+    public List<NoticeDto> getDepartmentNotice(@PathVariable String department,
+                                               @RequestParam(name = "page", defaultValue = "1") int page,
+                                               @RequestParam(name = "size", defaultValue = "20") int size) {
+        List<NoticeDto> departmentNotices = departmentNoticeService.findDepartmentNotices(page, size, department);
 
-        return pageDepartmentNotices;
+        return departmentNotices ;
+    }
+
+    @GetMapping("/universityNotice")
+    public List<NoticeDto> getUniversityNotice(@RequestParam(name = "page", defaultValue = "1") int page,
+                                               @RequestParam(name = "size", defaultValue = "20") int size) {
+        List<NoticeDto> universityNotices = universityNoticeService.findUniversityNotices(page, size);
+
+        return universityNotices;
     }
 }
