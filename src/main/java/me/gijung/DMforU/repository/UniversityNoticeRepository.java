e73b1965187c0292d83728d162cf9faa8d97b896
GiJungPark <weeds2577@gmail.com>
2024-02-11 20:32:52 +0900

    style(UniversityNoticeRepository): 사용하지 않는 import문 삭제

--- src/main/java/me/gijung/DMforU/repository/UniversityNoticeRepository.java
+++ src/main/java/me/gijung/DMforU/repository/UniversityNoticeRepository.java
@@ -3,7 +3,6 @@ package me.gijung.DMforU.repository;
 import me.gijung.DMforU.model.entity.UniversityNotice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface UniversityNoticeRepository extends JpaRepository<UniversityNotice, Long> {
 
