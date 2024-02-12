e57b84b0d2df7e26725cd3dc4fb70c1c42e08c59
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-02-12 15:32:25 +0900

    주석 및 설명 추가

--- src/main/java/me/gijung/DMforU/controller/MessageController.java
+++ src/main/java/me/gijung/DMforU/controller/MessageController.java
@@ -2,13 +2,12 @@ package me.gijung.DMforU.controller;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.AllArgsConstructor;
-import me.gijung.DMforU.model.dto.MessageDto;
 import me.gijung.DMforU.model.dto.TokensDto;
+import me.gijung.DMforU.model.entity.DepartmentNotice;
+import me.gijung.DMforU.repository.DepartmentNoticeRepository;
 import me.gijung.DMforU.service.GoogleTokenService;
 import me.gijung.DMforU.service.RedisTokenService;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -17,34 +16,41 @@ public class MessageController {
     private final GoogleTokenService googleTokenService;
     private final RedisTokenService redisTokenService;
 
+    private final DepartmentNoticeRepository departmentNoticeRepository;
+
+
+    //직접 DB에 데이터 저장 ( 테스트를 위한 용토 )
     @PostMapping("/test")
-    public List<String> test(@RequestBody TokensDto tokensDto) throws FirebaseMessagingException {
-        redisTokenService.updateToken(tokensDto);
-        return null;
+    public void send_message(@RequestBody DepartmentNotice departmentNotice) throws FirebaseMessagingException {
+        System.out.println(departmentNotice.getTitle());
+        departmentNoticeRepository.save(departmentNotice);
     }
 
-
-    //알림 메세지 전송
-    //추후에 자동화 시킬 예정
-    @PostMapping("/send")
-    public void send_message(@RequestBody MessageDto messageDto) throws FirebaseMessagingException {
-        googleTokenService.send_message(messageDto.getTopic(), messageDto.getTitle(), messageDto.getBody());
-    }
-
-
+    /**
+     * 사용자 개인 설정 Topic 구독 API
+     * @RequestBody
+     *     private List<String> tokens;
+     *     private List<Topic> topic;
+     */
     //Topic 구독, 추후에 무결성 검증 로직 추가
-    //redisService.updateToken = Token 유효시간 갱신
+    //redisService.updateToken = Token 유효시간 갱신 및 Token Redis Server에 등록
     //messageService.updateToken = Google FCM token & Topic 등록
     @PostMapping("/update_topic")
     public void update_topic(@RequestBody TokensDto tokensDto) throws FirebaseMessagingException {
         redisTokenService.updateToken(tokensDto);
         googleTokenService.updateToken(tokensDto);
     }
-
+    /**
+     * 사용자 개인 설정 Topic 삭제 API
+     * @RequestBody
+     *     private List<String> tokens;
+     *     private List<Topic> topic;
+     */
     //Topic 구독 취소, 추후에 무결성 검증 로직 추가
     @PostMapping("/delete_topic")
     public void delete_topic(@RequestBody TokensDto tokensDto) throws FirebaseMessagingException {
         googleTokenService.deleteToken(tokensDto);
+        redisTokenService.deleteToken(tokensDto);
     }
 
 }
