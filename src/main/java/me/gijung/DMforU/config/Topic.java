f2137ac45b67f40e35bd63729127c81b3a2c9112
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-02-12 15:31:52 +0900

    Enum파일 구조 변경

--- src/main/java/me/gijung/DMforU/config/Topic.java
+++ src/main/java/me/gijung/DMforU/config/Topic.java
@@ -6,5 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Topic {
-    menu,schedule
+    menu("학식", "menu"),
+    schedule("일정", "menu");
+
+    private final String koreanName;
+    private final String englishName;
 }
