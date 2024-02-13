0de92197568c0c91bb4a4c7b9dff9efb368c1e40
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-02-13 15:55:29 +0900

    update

--- src/main/java/me/gijung/DMforU/Application.java
+++ src/main/java/me/gijung/DMforU/Application.java
@@ -3,10 +3,12 @@ package me.gijung.DMforU;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
