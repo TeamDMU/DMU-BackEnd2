4e70b0dbf67dd9dc0a66012e69357387c10cde76
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-02-13 15:57:06 +0900

    API 추가

--- src/main/java/me/gijung/DMforU/controller/CrawlingController.java
+++ src/main/java/me/gijung/DMforU/controller/CrawlingController.java
@@ -1,13 +1,19 @@
 package me.gijung.DMforU.controller;
 
 import lombok.RequiredArgsConstructor;
-import me.gijung.DMforU.model.dto.SchedulerDto;
-import me.gijung.DMforU.model.dto.DietDto;
+import me.gijung.DMforU.model.domain.Diet;
+import me.gijung.DMforU.model.domain.schedule.YearSchedule;
+import me.gijung.DMforU.model.dto.DepartmentNoticeDto;
+import me.gijung.DMforU.service.DepartmentNoticeService;
 import me.gijung.DMforU.service.DietService;
 import me.gijung.DMforU.service.SchedulerService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,14 +22,24 @@ public class CrawlingController {
 
     private final DietService dietService;
     private final SchedulerService schedulerService;
+    private final DepartmentNoticeService departmentNoticeService;
 
     @GetMapping("/diet")
-    public DietDto getDiet() {
+    public List<Diet> getDiet() {
         return dietService.getData();
     }
 
     @GetMapping("/scheduler")
-    public SchedulerDto getScheduler() {
+    public List<YearSchedule> getScheduler() {
         return schedulerService.getData();
+    }
+
+    @GetMapping("/departmentNotice/{department}")
+    public List<DepartmentNoticeDto> getDepartmentNotice(@PathVariable String department,
+                                                         @RequestParam(name = "page", defaultValue = "1") int page,
+                                                         @RequestParam(name = "size", defaultValue = "20") int size) {
+        List<DepartmentNoticeDto> pageDepartmentNotices = departmentNoticeService.findDepartmentNotices(page, size, department);
+
+        return pageDepartmentNotices;
     }
 }
