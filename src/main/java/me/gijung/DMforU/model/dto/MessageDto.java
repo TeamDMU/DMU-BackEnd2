12b33e4e91515255f3dd1cec37d3ed292558caa4
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-02-13 16:00:08 +0900

    주석 및 설명문 추가 리펙토링

---
+++ src/main/java/me/gijung/DMforU/model/dto/MessageDto.java
@@ -0,0 +1,22 @@
+package me.gijung.DMforU.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MessageDto {
+    private String topic;
+    private String title;
+    private String body;
+    public MessageDto(String topic, String body) {
+        this.topic = topic;
+        this.title = "키워드 알림 도착";
+        this.body ="'" + body + "'" + "키워드가 포함된 공지가 기다리고 있어요!";
+    }
+}
