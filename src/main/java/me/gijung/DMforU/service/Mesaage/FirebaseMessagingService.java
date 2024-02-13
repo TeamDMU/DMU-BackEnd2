b472e123b91fd3fd310c7e7da8c1bcae16b3018b
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-02-13 15:58:42 +0900

    Message 타입 변경

--- src/main/java/me/gijung/DMforU/service/Mesaage/FirebaseMessagingService.java
+++ src/main/java/me/gijung/DMforU/service/Mesaage/FirebaseMessagingService.java
@@ -1,6 +1,7 @@
 package me.gijung.DMforU.service.Mesaage;
 
 import com.google.firebase.messaging.Message;
+import me.gijung.DMforU.model.dto.MessageDto;
 
 import java.util.List;
 
@@ -8,6 +9,6 @@ public interface FirebaseMessagingService<T>{
 
     T getInstance();
 
-    Message sendMessage(String topic, String title, String body);
+    Message sendMessage(MessageDto messageDto);
 
 }
