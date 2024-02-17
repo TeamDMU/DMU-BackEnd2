146f4f2c3a793e373a5f71cf5a587d7d0a891a04
GiJungPark <weeds2577@gmail.com>
2024-02-18 05:45:32 +0900

    refactor(NoticeService): 변경된 repository를 서비스 로직에 전부 적용

--- src/main/java/me/gijung/DMforU/service/NoticeService.java
+++ src/main/java/me/gijung/DMforU/service/NoticeService.java
@@ -2,14 +2,15 @@ package me.gijung.DMforU.service;
 
 import lombok.RequiredArgsConstructor;
 import me.gijung.DMforU.model.dto.NoticeDto;
+import me.gijung.DMforU.model.entity.Notice;
 import me.gijung.DMforU.repository.NoticeRepository;
+import me.gijung.DMforU.utils.NoticeMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -27,27 +28,10 @@ public class NoticeService {
      * @return 페이지에 해당하는 공지사항 목록
      */
     public List<NoticeDto> getNotices(String keyword, int page, int size) {
-        Page<Object> result = noticeRepository.searchByTitleIgnoringSpaces(keyword,
+        Page<Notice> result = noticeRepository.findByTitleContainingKeyword(keyword,
                 PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, "date")));
 
-        return result.map(this::mapToDto).getContent();
+        return result.map(NoticeMapper::mapToDto).getContent();
     }
 
-    /**
-     * 대학 또는 학과 공지의 date, title, author, url 필드로 이뤄진 object를 dto로 변환한다.
-     *
-     * @param obj 검색된 Object(date, title, author, url 순으로 Object[]로 이뤄짐)
-     * @return 공지사항 dto
-     */
-    private NoticeDto mapToDto(Object obj) {
-        Object[] re = (Object[]) obj;
-        java.sql.Date sqlDate = (java.sql.Date) re[0];
-        LocalDate date = sqlDate.toLocalDate();
-        return NoticeDto.builder()
-                .date(date)
-                .title((String) re[1])
-                .author((String) re[2])
-                .url((String) re[3])
-                .build();
-    }
 }
