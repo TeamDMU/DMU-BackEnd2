7cc18d82088cc327b99153adb7c0731dc6992e87
GiJungPark <weeds2577@gmail.com>
2024-02-18 02:49:09 +0900

    feat(NoticeMapper): Mapper 인터페이스를 구현하는 NoticeMapper 작성

---
+++ src/main/java/me/gijung/DMforU/utils/NoticeMapper.java
@@ -0,0 +1,23 @@
+package me.gijung.DMforU.utils;
+
+import me.gijung.DMforU.model.dto.NoticeDto;
+import me.gijung.DMforU.model.entity.Notice;
+
+public class NoticeMapper implements Mapper<NoticeDto, Notice> {
+
+    /**
+     * 공지사항 엔티티를 dto로 변환한다.
+     *
+     * @param notice 공지사항 엔티티
+     * @return 공지사항 dto
+     */
+    @Override
+    public NoticeDto mapToDto(Notice notice) {
+        return NoticeDto.builder()
+                .date(notice.getDate())
+                .title(notice.getTitle())
+                .author(notice.getAuthor())
+                .url(notice.getUrl())
+                .build();
+    }
+}
