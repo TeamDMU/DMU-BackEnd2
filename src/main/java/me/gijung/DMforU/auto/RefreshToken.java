da7426f53d7d73149f50a5f61cd6422abbb8bb8e
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-06 15:24:20 +0900

    리펙토링

--- src/main/java/me/gijung/DMforU/auto/RefreshToken.java
+++ src/main/java/me/gijung/DMforU/auto/RefreshToken.java
@@ -2,29 +2,34 @@ package me.gijung.DMforU.auto;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 
 @Component
+@Slf4j
 public class RefreshToken {
-
-    private static final String SERVICE_ACCOUNT_KEY_FILE = "src/main/resources/key/fire-base-key.json";
-
-    //Servcer AccessToken 갱신
+    // Servcer AccessToken 갱신
     @Scheduled(fixedRate = 5000000)
     private void refreshAccessToken() throws IOException {
+        Resource resource = new ClassPathResource("key/fire-base-key.json");
+
+        InputStream inputStream = resource.getInputStream();
         GoogleCredentials credentials = ServiceAccountCredentials
-                .fromStream(new FileInputStream(SERVICE_ACCOUNT_KEY_FILE))
+                .fromStream(inputStream)
                 .createScoped(Collections.singletonList("https://www.googleapis.com/auth/firebase.messaging"));
         credentials.refreshIfExpired();
-        String tokenValue = credentials.getAccessToken().getTokenValue();
-        System.out.println("Refresh Token ::: " + tokenValue + "| Date ::: " +
-                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+
+//        log.info("Refresh_Token_log : " + LocalDateTime.now());
+
     }
 }
--- src/main/java/me/gijung/DMforU/service/RedisService.java
+++ src/main/java/me/gijung/DMforU/service/RedisService.java
@@ -2,6 +2,7 @@ package me.gijung.DMforU.service;
 
 import lombok.AllArgsConstructor;
 import me.gijung.DMforU.config.Topic;
+import me.gijung.DMforU.model.dto.DepartmentDto;
 import me.gijung.DMforU.model.dto.TokensDto;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,19 @@ import java.util.Date;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
 
 @Service
 @AllArgsConstructor
-public class RedisTokenService implements TokenService<TokensDto> {
+public class RedisService implements TokenService<TokensDto> {
 
 
     private RedisTemplate<String, String> redisTemplate;
+
+
+    public void updateDepartment(DepartmentDto dto) {
+
+    }
 
 
     /**
@@ -27,27 +34,26 @@ public class RedisTokenService implements TokenService<TokensDto> {
      * Value - List<Topic>
      */
     public void updateToken(TokensDto tokensDto) {
-        for (String token : tokensDto.getTokens()) {
-            EnumSet<Topic> topics = EnumSet.allOf(Topic.class);
-            List<Topic> topic1 = tokensDto.getTopic();
-            for (Topic topic : topics) {
-                if (topic1.contains(topic)) {
-                    redisTemplate.opsForSet().add(token, String.valueOf(topic));
-                    System.out.println("Redis Save Token :::" + token);
-                    redisTemplate.expire(token, 30, TimeUnit.SECONDS);
-                }
-            }
-        }
+        processToken(tokensDto, (token, topic) -> {
+            redisTemplate.opsForSet().add(token, String.valueOf(topic));
+            redisTemplate.expire(token, 30, TimeUnit.DAYS);
+        });
     }
 
     //Redis Server Token 삭제
     public void deleteToken(TokensDto tokensDto) {
+        processToken(tokensDto, (token, topic) -> {
+            redisTemplate.opsForSet().remove(token, String.valueOf(topic));
+        });
+    }
+
+    private void processToken(TokensDto tokensDto, BiConsumer<String, Topic> tokenProcessor) {
         for (String token : tokensDto.getTokens()) {
             EnumSet<Topic> topics = EnumSet.allOf(Topic.class);
             List<Topic> topic1 = tokensDto.getTopic();
             for (Topic topic : topics) {
                 if (topic1.contains(topic)) {
-                    redisTemplate.opsForSet().remove(token, String.valueOf(topic));
+                    tokenProcessor.accept(token, topic);
                 }
             }
         }
