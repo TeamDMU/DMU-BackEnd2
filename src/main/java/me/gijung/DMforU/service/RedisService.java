3ce18e22a2a3bed5f712deaae18bbc79f743675a
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-06 15:33:41 +0900

    주석문 추가

--- src/main/java/me/gijung/DMforU/service/RedisService.java
+++ src/main/java/me/gijung/DMforU/service/RedisService.java
@@ -1,10 +1,13 @@
 package me.gijung.DMforU.service;
 
 import lombok.AllArgsConstructor;
+import me.gijung.DMforU.config.Major;
 import me.gijung.DMforU.config.Topic;
 import me.gijung.DMforU.model.dto.DepartmentDto;
 import me.gijung.DMforU.model.dto.TokensDto;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 
@@ -22,11 +25,13 @@ public class RedisService implements TokenService<TokensDto> {
     private RedisTemplate<String, String> redisTemplate;
 
 
+    /**
+     * 클라이언트에서 학과 설정시 학과, 토큰을 
+     * Key - Value 로 Redis Server에 저장
+     */
     public void updateDepartment(DepartmentDto dto) {
-
+        redisTemplate.opsForList().rightPushAll(dto.getDepartment(), dto.getTokens());
     }
-
-
     /**
      * Redis Server Token 유효 시간 갱신 및 등록
      * Set
@@ -36,7 +41,7 @@ public class RedisService implements TokenService<TokensDto> {
     public void updateToken(TokensDto tokensDto) {
         processToken(tokensDto, (token, topic) -> {
             redisTemplate.opsForSet().add(token, String.valueOf(topic));
-            redisTemplate.expire(token, 30, TimeUnit.DAYS);
+            redisTemplate.expire(token, 30, TimeUnit.SECONDS);
         });
     }
 
@@ -46,6 +51,17 @@ public class RedisService implements TokenService<TokensDto> {
             redisTemplate.opsForSet().remove(token, String.valueOf(topic));
         });
     }
+
+//    public void deleteDepartment(List<String> tokens) {
+//        ListOperations<String, String> listOperations = redisTemplate.opsForList();
+//        Major[] values = Major.values();
+//        for (Major value : values) {
+//            List<String> range = listOperations.range(value.getNoticeCode(), 0, -1);
+//            for (String list : range) {
+//                        redisTemplate.opsForList().remove(list, 0, tokens);
+//            }
+//        }
+//    }
 
     private void processToken(TokensDto tokensDto, BiConsumer<String, Topic> tokenProcessor) {
         for (String token : tokensDto.getTokens()) {
