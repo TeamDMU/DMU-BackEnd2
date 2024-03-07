65a9204005146ffd4b469653b2ebd93b6a59ec50
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-07 20:03:52 +0900

    Google 관련 로직 리펙토링

--- src/main/java/me/gijung/DMforU/service/GoogleService.java
+++ src/main/java/me/gijung/DMforU/service/GoogleService.java
@@ -2,16 +2,24 @@ package me.gijung.DMforU.service;
 
 import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
-import me.gijung.DMforU.config.Topic;
 import me.gijung.DMforU.model.dto.TokensDto;
-import me.gijung.DMforU.service.Mesaage.FirebaseMessagingService;
+import me.gijung.DMforU.service.firebase.GoogleToken;
 import org.springframework.stereotype.Service;
 
-import java.util.EnumSet;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
-public class GoogleService{
+public class GoogleService {
+    private final GoogleToken googleToken;
+
+    public void updateToken(TokensDto tokensDto) throws FirebaseMessagingException {
+        googleToken.updateToken(tokensDto);
+    }
+
+    public void deleteToken(TokensDto tokensDto) throws FirebaseMessagingException {
+        googleToken.deleteToken(tokensDto);
+    }
+    public void AllDeleteTopic(TokensDto tokensDto) throws FirebaseMessagingException {
+        googleToken.AllDeleteTopic(tokensDto);
+    }
 }
