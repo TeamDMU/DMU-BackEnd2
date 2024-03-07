4ca69a2f0309981c9f80e44565ac8892b55cbccf
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-07 20:00:04 +0900

    리펙토링

--- src/main/java/me/gijung/DMforU/service/Mesaage/FirebaseMessagingService.java
+++ src/main/java/me/gijung/DMforU/service/Mesaage/FirebaseMessagingService.java
@@ -1,14 +1,7 @@
 package me.gijung.DMforU.service.Mesaage;
 
-import com.google.firebase.messaging.Message;
-import me.gijung.DMforU.model.dto.MessageDto;
-
-import java.util.List;
 
 public interface FirebaseMessagingService<T>{
 
     T getInstance();
-
-    Message sendMessage(MessageDto messageDto);
-
 }
