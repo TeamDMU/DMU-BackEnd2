c2d5628a4a23cbbdb5351ed7dddd0c430bdd159f
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-03-09 02:48:12 +0900

    refactor(NoticeDto): 불필요한 생성자 삭제

--- src/main/java/me/gijung/DMforU/model/dto/NoticeDto.java
+++ src/main/java/me/gijung/DMforU/model/dto/NoticeDto.java
@@ -3,26 +3,15 @@ package me.gijung.DMforU.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import me.gijung.DMforU.model.entity.Notice;
 
 import java.time.LocalDate;
 
-@Getter
 @Builder
+@Getter
 @AllArgsConstructor
 public class NoticeDto {
-
     private LocalDate date;
     private String title;
     private String author;
     private String url;
-    private String type;
-
-    public NoticeDto(Notice notice) {
-        this.title = notice.getTitle();
-        this.date = notice.getDate();
-        this.author = notice.getAuthor();
-        this.url = notice.getUrl();
-        this.type = notice.getType();
-    }
 }
