735df8cdb07c9eec3220f062fbb6cdf1b755dd44
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-03-09 02:44:28 +0900

    fix: 공지사항에서 type필드가 추가된 문제 해결

--- src/main/java/me/gijung/DMforU/service/MessageService.java
+++ src/main/java/me/gijung/DMforU/service/MessageService.java
@@ -7,10 +7,8 @@ import com.google.firebase.messaging.MulticastMessage;
 import lombok.AllArgsConstructor;
 import me.gijung.DMforU.config.Topic;
 import me.gijung.DMforU.model.dto.MessageDto;
-import me.gijung.DMforU.model.dto.NoticeDto;
 import me.gijung.DMforU.model.entity.Notice;
 import me.gijung.DMforU.service.Mesaage.Messaging;
-import me.gijung.DMforU.utils.NoticeMapper;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -24,26 +22,24 @@ public class MessageService {
     private Set<String> keys;
 
     public void send_message(Notice notice) throws FirebaseMessagingException {
-        NoticeDto noticeDto = new NoticeDto(notice);
         String value = null;
         keys = redisTemplate.keys("*");
 
         for (String key : keys) {
             Set<String> values = redisTemplate.opsForZSet().range(key, -1, -1);
-            noticeDto = NoticeMapper.mapToDto(notice);
             if (values != null && !values.isEmpty()) {
                 value = values.iterator().next();
             }
         }
 
         //학과 공지사항
-        if (value.equals(noticeDto.getType())) {
+        if (value.equals(notice.getType())) {
             DepartmentMessaging(keys);
         }
 
         //대학 공지사항
-        if (noticeDto.getType().equals("대학")) {
-            UniversityMessaging(noticeDto);
+        if (notice.getType().equals("대학")) {
+            UniversityMessaging(notice.getTitle());
         }
     }
 
@@ -54,12 +50,11 @@ public class MessageService {
     }
 
     //대학 공지사항
-    private void UniversityMessaging(NoticeDto noticeDto) throws FirebaseMessagingException {
+    private void UniversityMessaging(String noticeTitle) throws FirebaseMessagingException {
         Topic[] TopicValues = Topic.values();
         for (Topic topic : TopicValues) {
 
-            boolean contains = noticeDto
-                    .getTitle()
+            boolean contains = noticeTitle
                     .contains(String.valueOf(topic.getKoreanName()));
 
             if (contains) {
