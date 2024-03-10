ee5d692ea5854321b2f5fa9f5c034f360124e27b
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-10 14:52:26 +0900

    반복문 함수화 및 NoticeDTO -> TypeNoticeDTO 변경

--- src/main/java/me/gijung/DMforU/service/MessageService.java
+++ src/main/java/me/gijung/DMforU/service/MessageService.java
@@ -6,10 +6,9 @@ import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.MulticastMessage;
 import lombok.AllArgsConstructor;
 import me.gijung.DMforU.config.Topic;
+import me.gijung.DMforU.model.dto.TypeNoticeDto;
 import me.gijung.DMforU.model.dto.MessageDto;
-import me.gijung.DMforU.model.dto.NoticeDto;
 import me.gijung.DMforU.model.entity.Notice;
-import me.gijung.DMforU.service.Mesaage.FirebaseMessagingService;
 import me.gijung.DMforU.service.Mesaage.Messaging;
 import me.gijung.DMforU.utils.NoticeMapper;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -23,28 +22,34 @@ public class MessageService {
 
     private RedisTemplate<String, String> redisTemplate;
     private Set<String> keys;
+
     public void send_message(Notice notice) throws FirebaseMessagingException {
-        NoticeDto noticeDto = new NoticeDto(notice);
-        String value = null;
-        keys = redisTemplate.keys("*");
-            for (String key : keys) {
-                Set<String> values = redisTemplate.opsForZSet().range(key, -1, -1);
-                noticeDto = NoticeMapper.mapToDto(notice);
-                if (values != null && !values.isEmpty()) {
-                    value = values.iterator().next();
-                }
-            }
+        TypeNoticeDto typeNoticeDto = NoticeMapper.maoToDepartmentNotice(notice);
+        String departmentValue = find_Department_value();
         //학과 공지사항
-        if (value.equals(noticeDto.getType())) {
+        if (departmentValue.equals(typeNoticeDto.getType())) {
             DepartmentMessaging(keys);
         }
         //대학 공지사항
-        if(noticeDto.getType().equals("대학")){
-            UniversityMessaging(noticeDto);
+        if(typeNoticeDto.getType().equals("대학")){
+            UniversityMessaging(typeNoticeDto);
         }
 
     }
-    
+
+    //학과 Type 검색 [ In Redis ]
+    private String find_Department_value() {
+        String value = null;
+        keys = redisTemplate.keys("*");
+        for (String key : keys) {
+            Set<String> values = redisTemplate.opsForZSet().range(key, -1, -1);
+            if (values != null && !values.isEmpty()) {
+                value = values.iterator().next();
+            }
+        }
+        return value;
+    }
+
     //학과 공지사항
     private void DepartmentMessaging(Set<String> keys) throws FirebaseMessagingException {
         MulticastMessage multicastMessage = Messaging.sendMessage(keys);
@@ -52,11 +57,11 @@ public class MessageService {
     }
 
     //대학 공지사항
-    private void UniversityMessaging(NoticeDto noticeDto) throws FirebaseMessagingException {
+    private void UniversityMessaging(TypeNoticeDto typeNoticeDto) throws FirebaseMessagingException {
         Topic[] TopicValues = Topic.values();
         for (Topic topic : TopicValues) {
 
-            boolean contains = noticeDto
+            boolean contains = typeNoticeDto
                     .getTitle()
                     .contains(String.valueOf(topic.getKoreanName()));
             if (contains) {
