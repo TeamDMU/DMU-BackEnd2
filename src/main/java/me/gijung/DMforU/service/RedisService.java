9989488e75fb697248fdd32521cc19aeec61b7fe
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-10 14:53:07 +0900

    리펙토링

--- src/main/java/me/gijung/DMforU/service/RedisService.java
+++ src/main/java/me/gijung/DMforU/service/RedisService.java
@@ -14,13 +14,18 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class RedisService{
+
     private final Token token;
 
     public void updateToken(TokensDto tokensDto){
-        token.updateToken(tokensDto);
+
+        token.update_Token(tokensDto);
+
     }
 
     public void deleteToken(TokensDto tokensDto){
-        token.deleteToken(tokensDto);
+
+        token.delete_Token(tokensDto);
+
     }
 }
