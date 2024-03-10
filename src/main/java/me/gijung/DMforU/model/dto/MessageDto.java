d4f672266f62963d53eb75b73e1091d78ea4f2f5
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-10 14:47:02 +0900

    어노테이션 정리 및 default Message 수정

--- src/main/java/me/gijung/DMforU/model/dto/MessageDto.java
+++ src/main/java/me/gijung/DMforU/model/dto/MessageDto.java
@@ -1,23 +1,22 @@
 package me.gijung.DMforU.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
-@Builder
-@AllArgsConstructor
-public class
-MessageDto {
+@NoArgsConstructor
+public class MessageDto {
+
+    private final String setTitle = "키워드 알림 도착";
+    private final String setBody = " 키워드가 포함된 공지가 기다리고 있어요!";
+
     private String topic;
-    private String title;
     private String body;
-    public MessageDto(String topic, String body) {
+
+    @Builder
+    public MessageDto(String topic, String Inbody) {
         this.topic = topic;
-        this.title = "키워드 알림 도착";
-        this.body ="'" + body + "'" + " 키워드가 포함된 공지가 기다리고 있어요!";
+        this.body = Inbody + setBody;
     }
 }
