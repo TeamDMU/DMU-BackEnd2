c3f9010c3147f5c6540fbe680d82871bf24a8445
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-10 15:11:21 +0900

    주석문 정리

--- src/main/java/me/gijung/DMforU/controller/TokenController.java
+++ src/main/java/me/gijung/DMforU/controller/TokenController.java
@@ -14,10 +14,10 @@ public class TokenController {
     private final GoogleService googleService;
     private final RedisService redisService;
 
-    //Topic 구독, 추후에 무결성 검증 로직 추가
+    //Topic 구독, 추후에 무결성 검증 로직 추가!
     //redisService.updateToken = Token 유효시간 갱신 및 Token Redis Server에 등록
     //messageService.updateToken = Google FCM token & Topic 등록
-    @PostMapping("/update_topic")
+    @PostMapping("/updagte_topic")
     public void update_topic(@RequestBody TokensDto tokensDto){
 
         redisService.updateToken(tokensDto);
