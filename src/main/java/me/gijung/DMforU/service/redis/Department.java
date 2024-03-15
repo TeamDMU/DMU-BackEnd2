e90a97644251333a42b5991c40b59c417518df99
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-15 22:18:49 +0900

    score 적용 -> 학과 데이터 맨 앞 배치

--- src/main/java/me/gijung/DMforU/service/redis/Department.java
+++ src/main/java/me/gijung/DMforU/service/redis/Department.java
@@ -1,6 +1,8 @@
 package me.gijung.DMforU.service.redis;
 
 import lombok.RequiredArgsConstructor;
+import me.gijung.DMforU.config.Major;
+import me.gijung.DMforU.config.Topic;
 import me.gijung.DMforU.model.dto.DepartmentDto;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -9,11 +11,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class Department {
 
+    private static String englishName;
+
     private final RedisTemplate<String, String> redisTemplate;
 
     public void update_department(DepartmentDto departmentDto) {
         for (String token : departmentDto.getTokens()) {
-            redisTemplate.opsForZSet().add(token,departmentDto.getDepartment(), 100);
+            redisTemplate.opsForZSet().add(token,departmentDto.getDepartment(), -2);
 //            redisTemplate.expire(token, 100, TimeUnit.SECONDS);
         }
     }
