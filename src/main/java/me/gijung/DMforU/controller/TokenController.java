5b18dda8ef53abe54239961af9e86e32639b8b88
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-15 22:17:44 +0900

    update, delete 로직 통합 -> update

--- src/main/java/me/gijung/DMforU/controller/TokenController.java
+++ src/main/java/me/gijung/DMforU/controller/TokenController.java
@@ -1,9 +1,12 @@
 package me.gijung.DMforU.controller;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import me.gijung.DMforU.model.dto.TokensDto;
-import me.gijung.DMforU.service.token.GoogleService;
-import me.gijung.DMforU.service.token.RedisService;
+import me.gijung.DMforU.model.entity.Notice;
+import me.gijung.DMforU.repository.NoticeRepository;
+import me.gijung.DMforU.service.TokenService;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,26 +14,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/token/v1/dmu")
 public class TokenController {
 
-    private final GoogleService googleService;
-    private final RedisService redisService;
+    private final TokenService tokenService;
+
+
 
     //redisService.updateToken = Token 유효시간 갱신 및 Token Redis Server에 등록
     //messageService.updateToken = Google FCM token & Topic 등록
     @PostMapping("/update_topic")
-    public void update_topic(@RequestBody TokensDto tokensDto){
 
-        redisService.update_Token(tokensDto);
-        googleService.update_Token(tokensDto);
+    public void update_topic(@RequestBody TokensDto tokensDto) {
+
+        tokenService.update_token(tokensDto);
 
     }
 
 
     //Topic 구독 취소, 추후에 무결성 검증 로직 추가
     @PostMapping("/delete_topic")
-    public void delete_topic(@RequestBody TokensDto tokensDto){
+    public void delete_topic(@RequestBody TokensDto tokensDto) {
 
-        googleService.delete_Token(tokensDto);
-        redisService.delete_Token(tokensDto);
+        tokenService.delete_token(tokensDto);
+
 
     }
 }
