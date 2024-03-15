40bd2c9236493fe4d97b5f078dae1a89ea53161c
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-15 22:16:56 +0900

    Redis Template 설정 적용

--- src/main/java/me/gijung/DMforU/config/RedisConfiguration.java
+++ src/main/java/me/gijung/DMforU/config/RedisConfiguration.java
@@ -1,39 +1,38 @@
-//package me.gijung.DMforU.config;
-//
-//import com.google.firebase.messaging.FirebaseMessagingException;
-//import lombok.RequiredArgsConstructor;
-//import me.gijung.DMforU.model.dto.TokensDto;
-//import me.gijung.DMforU.service.token.GoogleService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.Message;
-//import org.springframework.data.redis.connection.MessageListener;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.listener.PatternTopic;
-//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//import org.springframework.beans.factory.annotation.Value;
-//import java.util.Collections;
-//@Configuration
-//@RequiredArgsConstructor
-//public class RedisConfiguration {
-//
+package me.gijung.DMforU.config;
+
+import com.google.firebase.messaging.FirebaseMessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.Collections;
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfiguration {
+
 //    private final GoogleService googleService;
-//
-//    @Value("${spring.data.redis.host}")
-//    private String host;
-//
-//    @Value("${spring.data.redis.port}")
-//    private int port;
-//
-//    @Bean
-//    public RedisConnectionFactory redisConnectionFactory(){
-//
-//        return new LettuceConnectionFactory(host, port);
-//    }
-//
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        return new LettuceConnectionFactory(host, port);
+    }
+
 //    //Token 만료시 자동으로 Google FCM Server에 Token삭제 요청 전송
 //    @Bean
 //    RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
@@ -57,15 +56,17 @@
 //        }, new PatternTopic("__keyevent@*__:expired"));
 //        return container;
 //    }
-//
-//    // RedisTemplate 빈 설정
-//    // 직렬화 하지 않으면 cli모드에서 데이터 조회시 데이터 깨짐 현상
-//    @Bean
-//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-//        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(connectionFactory);
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new StringRedisSerializer()); // 더 적합한 직렬화 방식 선택 가능
-//        return redisTemplate;
-//    }
-//}
+
+    // RedisTemplate 빈 설정
+    // 직렬화 하지 않으면 cli모드에서 데이터 조회시 데이터 깨짐 현상
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());   // Key: String
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));  // Value: 직렬화에 사용할 Object 사용하기
+        return redisTemplate;
+    }
+}
---
+++ src/main/java/me/gijung/DMforU/service/TokenService.java
@@ -0,0 +1,2 @@
+package me.gijung.DMforU.service;public class TokenService {
+}
