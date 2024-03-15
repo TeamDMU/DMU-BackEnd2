84a835fbf4fe4573aaf5421c980b3f5f76774abe
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-15 22:20:49 +0900

    token 관련 service 신규 파일 생성

--- src/main/java/me/gijung/DMforU/service/TokenService.java
+++ src/main/java/me/gijung/DMforU/service/TokenService.java
@@ -1,2 +1,28 @@
-package me.gijung.DMforU.service;public class TokenService {
+package me.gijung.DMforU.service;
+
+import lombok.RequiredArgsConstructor;
+import me.gijung.DMforU.model.dto.TokensDto;
+import me.gijung.DMforU.service.token.GoogleToken;
+import me.gijung.DMforU.service.token.RedisToken;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final GoogleToken googleToken;
+    private final RedisToken redisToken;
+
+    public void update_token(TokensDto tokensDto) {
+
+
+        redisToken.update_Token(tokensDto);
+        googleToken.update_Token(tokensDto);
+
+    }
+
+    public void delete_token(TokensDto tokensDto) {
+        redisToken.delete_Token(tokensDto);
+        googleToken.delete_Token(tokensDto);
+    }
 }
