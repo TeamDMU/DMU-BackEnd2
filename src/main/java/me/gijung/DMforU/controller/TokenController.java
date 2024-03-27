7fd50a07b32699a66911d1c179eba7d9aa446f39
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-27 14:58:52 +0900

    Token CRUD

--- src/main/java/me/gijung/DMforU/controller/TokenController.java
+++ src/main/java/me/gijung/DMforU/controller/TokenController.java
@@ -1,16 +1,12 @@
 package me.gijung.DMforU.controller;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import me.gijung.DMforU.model.dto.TokensDto;
-import me.gijung.DMforU.model.entity.Notice;
-import me.gijung.DMforU.repository.NoticeRepository;
-import me.gijung.DMforU.service.MessageService;
 import me.gijung.DMforU.service.TokenService;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @AllArgsConstructor
@@ -19,17 +15,28 @@ public class TokenController {
 
     private final TokenService tokenService;
 
+    //기존 Token TTL Refresh
+    @PostMapping("/refresh_token")
+    public void refreshTopic(@RequestBody TokensDto tokensDto){
+        tokenService.refreshToken(tokensDto);
+    }
+
+    //신규 Token 생성 URL
+    @PostMapping("/create_topic")
+    public void createTopic(@RequestBody TokensDto tokensDto){
+        tokenService.createToken(tokensDto);
+    }
+
     //redisService.updateToken = Token 유효시간 갱신 및 Token Redis Server에 등록
     //messageService.updateToken = Google FCM token & Topic 등록
     @PostMapping("/update_topic")
-    public void update_topic(@RequestBody TokensDto tokensDto) {
+    public void updateTopic(@RequestBody TokensDto tokensDto) throws FirebaseMessagingException, ExecutionException, InterruptedException {
         tokenService.updateToken(tokensDto);
     }
 
-
     //Topic 구독 취소, 추후에 무결성 검증 로직 추가
     @PostMapping("/delete_topic")
-    public void delete_topic(@RequestBody TokensDto tokensDto) {
+    public void deleteTopic(@RequestBody TokensDto tokensDto) {
         tokenService.deleteToken(tokensDto);
     }
 }
