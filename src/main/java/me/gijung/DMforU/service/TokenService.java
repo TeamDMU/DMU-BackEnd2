ae06a481fecaf87541f93430c3ccaf8ec80c801c
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-27 15:06:25 +0900

    외부 라이브러리 정리 및 로직 변경

--- src/main/java/me/gijung/DMforU/service/TokenService.java
+++ src/main/java/me/gijung/DMforU/service/TokenService.java
@@ -3,34 +3,35 @@ package me.gijung.DMforU.service;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
 import me.gijung.DMforU.model.dto.TokensDto;
-import me.gijung.DMforU.service.token.GoogleToken;
-import me.gijung.DMforU.service.token.RedisToken;
+
+import me.gijung.DMforU.service.token.Token;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
 
 @Service
 @RequiredArgsConstructor
 public class TokenService {
 
-    private final GoogleToken googleToken;
-    private final RedisToken redisToken;
+    private final Token<TokensDto> googleToken;
+    private final Token<TokensDto> redisToken;
 
-    public void updateToken(TokensDto tokensDto) {
+    public void refreshToken(TokensDto tokensDto) {
+        redisToken.refreshToken(tokensDto);
+    }
 
+    public void createToken(TokensDto tokensDto) {
+        redisToken.createToken(tokensDto);
+        googleToken.createToken(tokensDto);
+    }
+
+    public void updateToken(TokensDto tokensDto) throws FirebaseMessagingException, ExecutionException, InterruptedException {
         redisToken.updateToken(tokensDto);
         googleToken.updateToken(tokensDto);
-
     }
 
     public void deleteToken(TokensDto tokensDto) {
-
         redisToken.deleteToken(tokensDto);
         googleToken.deleteToken(tokensDto);
-
-    }
-
-    public void allDeleteToken(TokensDto tokensDto) throws FirebaseMessagingException {
-
-        googleToken.AllDeleteTopic(tokensDto);
-
     }
 }
