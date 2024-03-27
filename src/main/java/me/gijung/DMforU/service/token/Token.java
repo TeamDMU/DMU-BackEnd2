3786c5b8b7f36feabaef567b1ed8c571d9112ada
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-27 15:03:46 +0900

    Token 관련 메소드 정리

--- src/main/java/me/gijung/DMforU/service/token/Token.java
+++ src/main/java/me/gijung/DMforU/service/token/Token.java
@@ -1,10 +1,20 @@
 package me.gijung.DMforU.service.token;
+import com.google.firebase.messaging.FirebaseMessagingException;
 
+import java.util.concurrent.ExecutionException;
 
 public interface Token<T> {
 
-    void updateToken(T t);
+    void updateToken(T t) throws FirebaseMessagingException, ExecutionException, InterruptedException;
 
     void deleteToken(T t);
+
+    void createToken(T t);
+
+    default void refreshToken(T t) {}
+
+    default void selectDeleteTopic(T t) throws FirebaseMessagingException {
+    }
+
 
 }
