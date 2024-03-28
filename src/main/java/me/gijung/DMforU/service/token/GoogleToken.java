4a2b289d183f83912f63fcc1854229316850fe8e
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-28 18:49:24 +0900

    DTO 분리에 따른 로직 변경

--- src/main/java/me/gijung/DMforU/service/token/GoogleToken.java
+++ src/main/java/me/gijung/DMforU/service/token/GoogleToken.java
@@ -4,7 +4,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
 import me.gijung.DMforU.config.Topic;
-import me.gijung.DMforU.model.dto.TokensDto;
+import me.gijung.DMforU.model.dto.ServiceTokensDto;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +14,11 @@ import java.util.function.BiConsumer;
 @Service
 @Qualifier("googleToken")
 @RequiredArgsConstructor
-public class GoogleToken implements Token<TokensDto> {
+public class GoogleToken implements Token<ServiceTokensDto> {
 
 
     //Google FCM서버에 기기별 구독 업데이트
-    public void createToken(TokensDto tokensDto) {
+    public void createToken(ServiceTokensDto tokensDto) {
         firebaseSendToken(tokensDto, (tokens, topic) -> {
             try {
                 FirebaseMessaging.getInstance().subscribeToTopic(tokens, String.valueOf(topic));
@@ -28,7 +28,7 @@ public class GoogleToken implements Token<TokensDto> {
         });
     }
 
-    public void updateToken(TokensDto tokensDto) throws FirebaseMessagingException {
+    public void updateToken(ServiceTokensDto tokensDto) throws FirebaseMessagingException {
         deleteAllTopic(tokensDto);
         firebaseSendToken(tokensDto, (tokens, topic) -> {
             try {
@@ -40,7 +40,7 @@ public class GoogleToken implements Token<TokensDto> {
     }
 
     //Google FCM서버에 기기별 구독 삭제
-    public void deleteToken(TokensDto tokensDto){
+    public void deleteToken(ServiceTokensDto tokensDto){
         firebaseSendToken(tokensDto, (tokens, topic) -> {
             try {
                 FirebaseMessaging.getInstance().unsubscribeFromTopic(tokens, String.valueOf(topic));
@@ -50,19 +50,19 @@ public class GoogleToken implements Token<TokensDto> {
         });
     }
 
-    private void firebaseSendToken(TokensDto tokensDto, BiConsumer<List<String>, Topic> tokenProcesss) {
+    private void firebaseSendToken(ServiceTokensDto tokensDto, BiConsumer<List<String>, Topic> tokenProcesss) {
         EnumSet<Topic> topics = EnumSet.allOf(Topic.class);
         List<Topic> topic1 = tokensDto.getTopic();
         for (Topic topic : topics) {
             if (topic1.contains(topic)) {
-                tokenProcesss.accept(tokensDto.getTokens(), topic);
+                tokenProcesss.accept(Collections.singletonList(tokensDto.getTokens()), topic);
             }
         }
     }
 
-    private synchronized void deleteAllTopic(TokensDto tokensDto) throws FirebaseMessagingException {
+    private synchronized void deleteAllTopic(ServiceTokensDto tokensDto) throws FirebaseMessagingException {
         for (Topic value : Topic.values()) {
-            FirebaseMessaging.getInstance().unsubscribeFromTopic(tokensDto.getTokens(), String.valueOf(value));
+            FirebaseMessaging.getInstance().unsubscribeFromTopic(Collections.singletonList(tokensDto.getTokens()), String.valueOf(value));
         }
     }
 }
