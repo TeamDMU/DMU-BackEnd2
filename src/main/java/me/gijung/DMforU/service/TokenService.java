96e44a60d8d143d8f59ea68d629cbbb61b9fc5fd
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-28 18:58:17 +0900

    DTO 분리에 따른 매개변수, 로직 변경

--- src/main/java/me/gijung/DMforU/service/TokenService.java
+++ src/main/java/me/gijung/DMforU/service/TokenService.java
@@ -2,36 +2,39 @@ package me.gijung.DMforU.service;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
-import me.gijung.DMforU.model.dto.TokensDto;
+import me.gijung.DMforU.model.dto.RequestTokenDto;
+import me.gijung.DMforU.model.dto.ServiceTokensDto;
 
 import me.gijung.DMforU.service.token.Token;
 import org.springframework.stereotype.Service;
-
 import java.util.concurrent.ExecutionException;
+
+import static me.gijung.DMforU.utils.NoticeMapper.RequestTokenDtoToServiceTokensDto;
 
 @Service
 @RequiredArgsConstructor
 public class TokenService {
 
-    private final Token<TokensDto> googleToken;
-    private final Token<TokensDto> redisToken;
+    private final Token<ServiceTokensDto> googleToken;
+    private final Token<ServiceTokensDto> redisToken;
 
-    public void refreshToken(TokensDto tokensDto) {
-        redisToken.refreshToken(tokensDto);
+    //토큰 생명주기 연장
+    public void refreshToken(RequestTokenDto tokensDto) {
+        redisToken.refreshToken(mapToRequestTokenDto(tokensDto));
     }
 
-    public void createToken(TokensDto tokensDto) {
-        redisToken.createToken(tokensDto);
-        googleToken.createToken(tokensDto);
+    //토픽 업데이트
+    public void updateTopic(RequestTokenDto tokensDto) throws FirebaseMessagingException, ExecutionException, InterruptedException {
+        redisToken.updateToken(mapToRequestTokenDto(tokensDto));
+        googleToken.updateToken(mapToRequestTokenDto(tokensDto));
     }
 
-    public void updateToken(TokensDto tokensDto) throws FirebaseMessagingException, ExecutionException, InterruptedException {
-        redisToken.updateToken(tokensDto);
-        googleToken.updateToken(tokensDto);
+    public void deleteTopic(RequestTokenDto tokensDto) {
+        redisToken.deleteToken(mapToRequestTokenDto(tokensDto));
+        googleToken.deleteToken(mapToRequestTokenDto(tokensDto));
     }
 
-    public void deleteToken(TokensDto tokensDto) {
-        redisToken.deleteToken(tokensDto);
-        googleToken.deleteToken(tokensDto);
+    private ServiceTokensDto mapToRequestTokenDto(RequestTokenDto requestTokenDto) {
+        return RequestTokenDtoToServiceTokensDto(requestTokenDto);
     }
 }
