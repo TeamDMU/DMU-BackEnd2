88b938ecf9edca9fb87eeab3ef26b4a9624e0800
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-03-28 18:42:58 +0900

    모든 서비스 -> 특정 서비스에 적용

--- src/main/java/me/gijung/DMforU/model/dto/ServiceDepartmentDto.java
+++ src/main/java/me/gijung/DMforU/model/dto/ServiceDepartmentDto.java
@@ -1,17 +1,17 @@
 package me.gijung.DMforU.model.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import me.gijung.DMforU.config.Topic;
 
 import java.util.List;
 
-@Getter
 @Builder
+@Getter
 @AllArgsConstructor
-public class TokensDto {
-
+public class ServiceDepartmentDto {
     private List<String> tokens;
-    private List<Topic> topic;
+    private String department;
+
 }
--- src/main/java/me/gijung/DMforU/model/dto/ServiceTokensDto.java
+++ src/main/java/me/gijung/DMforU/model/dto/ServiceTokensDto.java
@@ -1,17 +1,17 @@
 package me.gijung.DMforU.model.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import me.gijung.DMforU.config.Topic;
 
 import java.util.List;
 
-@Builder
 @Getter
+@Builder
 @AllArgsConstructor
-public class DepartmentDto {
-    private List<String> tokens;
-    private String department;
+public class ServiceTokensDto {
 
+    private String tokens;
+    private List<Topic> topic;
 }
