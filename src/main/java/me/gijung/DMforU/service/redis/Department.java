e3d664753e92025bb3523bc6e7b09a7e5d8b88e6
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-04-01 14:13:41 +0900

    학과 삭제시 기본 학과 존재 하도록 로직 변경

--- src/main/java/me/gijung/DMforU/service/redis/Department.java
+++ src/main/java/me/gijung/DMforU/service/redis/Department.java
@@ -9,13 +9,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class Department {
 
+    private final static String DEFAULT_DEPARTMENT = " DEFAULT";
     private final RedisTemplate<String, String> redisTemplate;
-
 
     public void createDepartment(DepartmentDto departmentDto) {
             redisTemplate.opsForZSet().add(departmentDto.getToken(),departmentDto.getDepartment(), -2);
     }
-
 
     public void updateDepartment(DepartmentDto departmentDto) {
             redisTemplate.opsForZSet().removeRange(departmentDto.getToken(), 0, 0);
@@ -24,5 +23,6 @@ public class Department {
 
     public void deleteDepartment(DepartmentDto departmentDto) {
             redisTemplate.opsForZSet().remove(departmentDto.getToken(),departmentDto.getDepartment());
+            redisTemplate.opsForZSet().add(departmentDto.getToken(),DEFAULT_DEPARTMENT, -2);
     }
 }
