7dcd4a3cf84c3b9984591021a67074b402573fd0
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-04-01 14:14:35 +0900

    리펙토링

--- src/main/java/me/gijung/DMforU/service/TokenService.java
+++ src/main/java/me/gijung/DMforU/service/TokenService.java
@@ -6,6 +6,7 @@ import me.gijung.DMforU.model.dto.RequestTokenDto;
 
 import me.gijung.DMforU.model.dto.TokensDto;
 import me.gijung.DMforU.service.token.Token;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import java.util.concurrent.ExecutionException;
 
@@ -23,7 +24,9 @@ public class TokenService {
         redisToken.refreshToken(mapToRequestTokenDto(tokensDto));
     }
 
+
     //토픽 업데이트
+//    @Async("customThreadPool")
     public void updateTopic(RequestTokenDto tokensDto) throws FirebaseMessagingException, ExecutionException, InterruptedException {
         redisToken.updateToken(mapToRequestTokenDto(tokensDto));
         googleToken.updateToken(mapToRequestTokenDto(tokensDto));
