0cba7e8b491a4e48b5069eae0d586ad37929da2a
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-05-09 03:31:42 +0900

    refactor: Schedule 하드코딩한 문자열 상수화

--- src/main/java/com/dmforu/crawling/parser/ScheduleParser.java
+++ src/main/java/com/dmforu/crawling/parser/ScheduleParser.java
@@ -18,7 +18,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ScheduleParser implements Parser<Schedule.Year> {
 
-    private final String DMU_SCHEDULE_URL = "https://www.dongyang.ac.kr/dongyang/71/subview.do?year=";
+    private static final String DMU_SCHEDULE_URL = "https://www.dongyang.ac.kr/dongyang/71/subview.do?year=";
+    private static final String YEAR_SCHEDULE_SELECTOR = ".yearSchdulWrap";
+    private static final String MONTH_DATE_SELECTOR = "p";
+    private static final String MONTH_SCHEDULE_SELECTOR = ".scheList li";
+    private static final String SCHEDULE_DATE_SELECTOR = "dt span";
+    private static final String SCHEDULE_CONTENT_SELECTOR = "dd span";
 
     @Value("${server.time.zone}")
     private String TIME_ZONE;
@@ -43,7 +48,7 @@ public class ScheduleParser implements Parser<Schedule.Year> {
 
         Document document = WebPageLoader.getHTML(DMU_SCHEDULE_URL + year);
 
-        Elements monthTables = document.select(".yearSchdulWrap");
+        Elements monthTables = document.select(YEAR_SCHEDULE_SELECTOR);
 
         for (Element monthTable : monthTables) {
             Schedule.Month monthSchedule = fetchMonthSchedule(monthTable);
@@ -62,10 +67,10 @@ public class ScheduleParser implements Parser<Schedule.Year> {
 
         // <p id="yearmonth20241">2024.1</p> p태그에서 "2024.1"을 문자열로 가져온다.
         // 이때, 월 정보만 필요하기 때문에 문자열 인덱스 5부터의 정보만을 가져온다.
-        String monthText = monthTable.select("p").first().text().substring(5);
+        String monthText = monthTable.select(MONTH_DATE_SELECTOR).first().text().substring(5);
         int month = Integer.parseInt(monthText);
 
-        Elements scheduleTable = monthTable.select(".scheList li");
+        Elements scheduleTable = monthTable.select(MONTH_SCHEDULE_SELECTOR);
         for (Element schedule : scheduleTable) {
             monthSchedule.add(parseSchedule(schedule));
         }
@@ -73,11 +78,20 @@ public class ScheduleParser implements Parser<Schedule.Year> {
         return new Schedule.Month(month, monthSchedule);
     }
 
+    /**
+     * 다음과 같이 일정을 파싱한다.
+     *
+     * 01.01 (월)	신정
+     * -> ["01.01 (월)", "01.01 (월)"], "신정"
+     *
+     * 01.03 (수) ~ 01.15 (월)	정시모집 원서 접수
+     * -> ["01.03 (수)", "01.15 (월)"], "정시모집 원서 접수"
+     */
     private Schedule parseSchedule(Element schedule) {
-        String dateText = schedule.select("dt span").text().replaceAll(" ", "");
+        String dateText = schedule.select(SCHEDULE_DATE_SELECTOR).text().replaceAll(" ", "");
         String[] dates = dateText.contains("~") ? dateText.split("~") : new String[]{dateText, dateText};
 
-        String content = schedule.select("dd span").text();
+        String content = schedule.select(SCHEDULE_CONTENT_SELECTOR).text();
 
         return new Schedule(dates, content);
     }
