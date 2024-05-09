75eca5873d8b648aff04771836ca83f1a1a443bb
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-05-10 01:40:59 +0900

    refactor: Notice 구버전 API 복구 및 Swagger 작성
    - Notice 구버전 API 복구
    - Swagger API, Schema의 name, description, example 작성

--- src/main/java/com/dmforu/notice/dto/NoticeRequest.java
+++ src/main/java/com/dmforu/notice/dto/NoticeRequest.java
@@ -1,6 +1,7 @@
 package com.dmforu.notice.dto;
 
 import com.dmforu.notice.Notice;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,9 +13,16 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class NoticeRequest {
 
+    @Schema(description = "날짜", example = "2024-05-09")
     private LocalDate date;
+
+    @Schema(description = "날짜", example = "시흥시인재양성재단 24년 상반기 장학생 선발 안내")
     private String title;
+
+    @Schema(description = "작성자", example = "최지예")
     private String author;
+
+    @Schema(description = "URL", example = "https://www.dongyang.ac.kr/bbs/dongyang/7/124420/artclView.do?layout=unknown")
     private String url;
 
     public static NoticeRequest toDto(Notice notice) {
