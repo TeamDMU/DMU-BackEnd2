6492aa24a4b8035eec47fbd7e05272dc726fddf4
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-07-03 22:16:17 +0900

    feat: 주석 메세지 추가

--- src/main/java/com/dmforu/v2_messaging/util/MessagingV2.java
+++ src/main/java/com/dmforu/v2_messaging/util/MessagingV2.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class MessagingV2 {
 
-    //학과 메시지 툴 작성
+    //메시지 툴 작성
     public static MulticastMessage buildMessage(MessageDtoV2 messageDtoV2, List<String> tokenList, String type) {
         return MulticastMessage.builder()
                 .setNotification(Notification.builder()
@@ -18,20 +18,6 @@ public class MessagingV2 {
                 .addAllTokens(tokenList)
                 .putData("url", messageDtoV2.getUrl())
                 .putData("type", type)
-                .build();
-    }
-
-    //대학 메시지 툴 작성
-    public static MulticastMessage buildMessage(MessageDtoV2 messageDtoV2, List<String> tokenList, String type, String keyWordList) {
-        return MulticastMessage.builder()
-                .setNotification(Notification.builder()
-                        .setTitle(messageDtoV2.getTitle())
-                        .setBody(messageDtoV2.getBody())
-                        .build())
-                .addAllTokens(tokenList)
-                .putData("url", messageDtoV2.getUrl())
-                .putData("type", type)
-                .putData("keyWordList", keyWordList)
                 .build();
     }
 }
