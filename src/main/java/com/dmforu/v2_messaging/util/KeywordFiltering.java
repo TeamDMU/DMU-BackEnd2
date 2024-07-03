bb52c567ca5f7fa24102c833592b5d9ae4caaecf
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-07-03 22:15:57 +0900

    add: 필터링 기능 추가

--- src/main/java/com/dmforu/v2_messaging/util/KeywordFiltering.java
+++ src/main/java/com/dmforu/v2_messaging/util/KeywordFiltering.java
@@ -2,21 +2,26 @@ package com.dmforu.v2_messaging.util;
 
 import com.dmforu.v2_subscrible.config.Keywords;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class KeywordFiltering {
 
 
     //Keyword 필터링 [ 예 : 중간고사 -> 시험, 기말고사 -> 시험 ]
-    public static String keywordFilter(String title) {
-        if (title.contains("중간고사") || title.contains("기말고사")) {
-            return "시험";
-        }else{
-            for (Keywords value : Keywords.values()) {
+    public static Set<String> keywordFilter(String title) {
+        Set<String> keywordList = new HashSet<>();
+          for (Keywords value : Keywords.values()) {
                 if (title.contains(value.getKoreanName())) {
-                    return value.getKoreanName();
+                    keywordList.add(value.getKoreanName());
                 }
             }
-        }
-        return "none";
+        if (title.contains("중간고사") || title.contains("기말고사"))
+            keywordList.add("시험");
+
+        return keywordList;
     }
 
 }
