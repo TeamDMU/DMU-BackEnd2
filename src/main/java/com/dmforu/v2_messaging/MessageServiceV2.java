83f9409465e6517d5b964572390df67747148942
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-07-03 22:17:36 +0900

    fix: 학과, 대학 메세지 메서드 로직 변경

--- src/main/java/com/dmforu/v2_messaging/MessageServiceV2.java
+++ src/main/java/com/dmforu/v2_messaging/MessageServiceV2.java
@@ -14,9 +14,7 @@ import com.google.firebase.messaging.MulticastMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -32,46 +30,38 @@ public class MessageServiceV2 {
     private void noticeType(Notice notice){
         NoticeDtoV2 noticeDtoV2 = mapToDTO.noticeToNoticeDTO(notice);
         if (noticeDtoV2.getType().equals("대학")) {
-            getUnibersityTokenList(noticeDtoV2);
+            unibersityMessage(noticeDtoV2);
         }else {
-            getDepartmentTokenList(noticeDtoV2);
+            departmentMessage(noticeDtoV2);
         }
     }
 
     //특정 학과가 있는 TokenList 조회 [ 학과 ]
-    private void getDepartmentTokenList(NoticeDtoV2 noticeDtoV2) {
-        List<Token> byDepartment = tokenRepository.getDepartment(noticeDtoV2.getType());
-        List<String> tokenList = new ArrayList<>();
-        for (Token token : byDepartment) {
-            tokenList.add(token.getToken());
-        }
-        departmentBuildMessaging(noticeDtoV2,tokenList);
+    private void departmentMessage(NoticeDtoV2 noticeDtoV2) {
+        List<String> tokenList = tokenRepository.getDepartment(noticeDtoV2.getType());
+        buildMessaging(noticeDtoV2,tokenList);
     }
 
     //특정 키워드가 속해있는 TokenList 조회 [ 대학 ]
-    private void getUnibersityTokenList(NoticeDtoV2 noticeDtoV2) {
-        List<String> tokenList = new ArrayList<>();
-        if (!KeywordFiltering.keywordFilter(noticeDtoV2.getTitle()).equals("none")){
-            String s = KeywordFiltering.keywordFilter(noticeDtoV2.getTitle());
-            List<Token> allByKeywordsListIn = tokenRepository.findByTokenInKeyword(s);
-            for (Token token : allByKeywordsListIn) {
-                tokenList.add(token.getToken());
-            }
-            unibersityBuildMessaging(noticeDtoV2, tokenList, s);
+    private void unibersityMessage(NoticeDtoV2 noticeDtoV2) {
+        Set<String> list = KeywordFiltering.keywordFilter(noticeDtoV2.getTitle());
+        for (String s : list) {
+            List<String> byTokenInKeyword = tokenRepository.findByTokenInKeyword(s);
+            buildMessaging(noticeDtoV2, byTokenInKeyword, s);
         }
     }
 
     //학과 메시지 작성
-    private void departmentBuildMessaging(NoticeDtoV2 noticeDtoV2, List<String> tokenList) {
+    private void buildMessaging(NoticeDtoV2 noticeDtoV2, List<String> tokenList) {
         MessageDtoV2 messagedto = new MessageDtoV2(noticeDtoV2);
         MulticastMessage message = MessagingV2.buildMessage(messagedto, tokenList, noticeDtoV2.getType());
 //        FirebaseMessaging.getInstance().send(message);
     }
 
     //대학 메시지 작성
-    private void unibersityBuildMessaging(NoticeDtoV2 noticeDtoV2, List<String> tokenList, String keyWord) {
+    private void buildMessaging(NoticeDtoV2 noticeDtoV2, List<String> tokenList, String keyWord) {
         MessageDtoV2 messagedto = new MessageDtoV2(noticeDtoV2, keyWord);
-        MulticastMessage message = MessagingV2.buildMessage(messagedto, tokenList, noticeDtoV2.getType(),keyWord);
+        MulticastMessage message = MessagingV2.buildMessage(messagedto, tokenList, noticeDtoV2.getType());
 //        FirebaseMessaging.getInstance().send(message);
     }
 
