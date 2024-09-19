03766af3ac747c35ac092f207f49f68da87c3dfe
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-09-19 18:55:47 +0900

    refactor: Lombok 제거 및 생성자 주입, 변수가 아닌 함수로 변경
    - 필드 주입의 형태를 나타내던 부분을 생성자 주입으로 변경
    - val 변수로 데이터를 받아오는 부분을 함수로 변경

--- src/main/java/com/dmforu/cafeteria/CafeteriaController.kt
+++ src/main/java/com/dmforu/cafeteria/CafeteriaController.kt
@@ -2,7 +2,6 @@ package com.dmforu.cafeteria
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import lombok.RequiredArgsConstructor
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,13 +9,14 @@ import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "식단")
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/v1/cafeteria")
-class CafeteriaController {
-    private val cafeteriaService: CafeteriaService? = null
+class CafeteriaController(
+        private val cafeteriaService: CafeteriaService
+) {
+    @GetMapping
+    @Operation(summary = "식단표 API", description = "금주의 식단을 출력한다.<br>식단표는 매주 일요일에 갱신된다.<br>만약, 공휴일인 경우는 빈 리스트를 출력한다.")
 
-    @get:GetMapping
-    @get:Operation(summary = "식단표 API", description = "금주의 식단을 출력한다.<br>식단표는 매주 일요일에 갱신된다.<br>만약, 공휴일인 경우는 빈 리스트를 출력한다.")
-    val diet: ResponseEntity<List<Diet>>
-        get() = ResponseEntity.ok().body(cafeteriaService!!.data)
+    fun getDiet(): ResponseEntity<List<Diet>> {
+        return ResponseEntity.ok().body(cafeteriaService.getData())
+    }
 }
--- src/main/java/com/dmforu/cafeteria/CafeteriaService.kt
+++ src/main/java/com/dmforu/cafeteria/CafeteriaService.kt
@@ -1,14 +1,13 @@
 package com.dmforu.cafeteria
 
 import com.dmforu.crawling.parser.Parser
-import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Service
 
 @Service
-@RequiredArgsConstructor
-class CafeteriaService {
-    private val cafeteriaParser: Parser<Diet>? = null
-
-    val data: List<Diet>
-        get() = cafeteriaParser!!.parse()
+class CafeteriaService(
+        private val cafeteriaParser: Parser<Diet>
+) {
+    fun getData(): List<Diet> {
+        return cafeteriaParser.parse()
+    }
 }
